b5566fb261a413cddd724cb74f27ab2c28da29bd
Stuart Baumann <stuart@stuartbaumann.com>
2018-12-01 17:51:46 +0000

    remove base dependencies to see if it breaks

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,19 @@
-using Documenter, MultivariateFunctions
+using Documenter
+using MultivariateFunctions
 
 makedocs(
     format = :html,
     sitename = "MultivariateFunctions",
-    modules = [MultivariateFunctions]
+    modules = [MultivariateFunctions],
+    doctest = true,
+    root    = "<current-directory>",
+    source  = "src",
+    build   = "build",
+    clean   = true,
 )
 
 deploydocs(
-    repo   = "github.com/s-baumann/MultivariateFunctions.jl",
+    repo   = "github.com/s-baumann/MultivariateFunctions.jl.git",
     julia  = "1.0",
     target = "build",
     deps   = nothing,
